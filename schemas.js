fda1fd68d8999ed9b7be00ad0935a8a7e2e836a3
remunds <raban.emunds@gmail.com>
2021-01-04 21:01:05 +0100

    wip taskgenerator
    wip generating tasks
    
    added imageSchema and /getImage route which sends an image as http response
    
    added /putLabel route and tests
    
    done
    
    wip
    
    wip
    
    wip
    
    wip
    
    donw
    
    review changes

--- schemas.js
+++ schemas.js
@@ -2,14 +2,36 @@ const mongoose = require('mongoose')
 
 // create schema with all needed fields
 const taskSchema = new mongoose.Schema({
-  title: String,
-  description: String
-})
+    //careful: title and imageId(if != null) need to be unique
+    title: String,
+    description: String,
+    imageId: String
+});
 
 const imageSchema = new mongoose.Schema({
   type: String,
   data: Buffer,
   label: []
+})
+
+const chunkSchema = new mongoose.Schema({
+    timestamp: Number,
+    downloads: Number,
+    files_id: String,
+    n: Number,
+    data: [Buffer],
+})
+
+const fileSchema = new mongoose.Schema({
+    downloads: Number,
+    length: Number,
+    chunkSize: Number,
+    uploadDate: Number,
+    md5: String,
+    filename: String,
+    contentType: String,
+    aliases: [String],
+    metadate: Buffer //any
 })
 
 /* add methods like this:
@@ -22,6 +44,8 @@ const imageSchema = new mongoose.Schema({
 //   console.log(greeting);
 // }
 
-// export the mongoose model with the name Task (creates collection tasks)
-module.exports.task = mongoose.model('Task', taskSchema)
-module.exports.image = mongoose.model('Image', imageSchema)
+//export the mongoose model with the name Task (creates collection tasks)
+module.exports.task = mongoose.model('Task', taskSchema);
+module.exports.image = mongoose.model('Image', imageSchema);
+module.exports.chunk = mongoose.model('Chunk', chunkSchema);
+module.exports.file = mongoose.model('File', fileSchema);
