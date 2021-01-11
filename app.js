b765361cce97d2740173708369706dcea0182b84
remunds <raban.emunds@gmail.com>
2021-01-11 15:46:05 +0100

    merge changes

--- app.js
+++ app.js
@@ -12,27 +12,15 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 const port = 8000
 
+const schemas = require('./schemas');
+const Task = schemas.task;
+
 //mongoose controls our mongodb
 const mongoose = require('mongoose');
 mongoose.connect(`mongodb://192.168.0.64/${boxName}`, {useNewUrlParser: true});
 
-//data
-var taskList; 
-var Task;
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-
-//Sending Tasks "function"
-db.once('open', () => {
-  // we're connected!
-  console.log('connected to db');
-
-  //access task from schemas.js
-  Task = require('./schemas').task;
-});
-
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -44,61 +32,27 @@ app.get('/getData', (req, res) => {
 })
 
 app.get('/api/getTasks', (req, res) => {
-  //a query would normally need to be executed after save has fully finished:
-  // TestTask.save().then(() => {//call find here})
-  // await could also work
   Task.find((err, tasks) => {
     if (err) return console.error(err);
     console.log(tasks);
-    taskList = tasks
+    res.send(tasks)
   });
-  var taskListJson = JSON.stringify(taskList)
-  res.send(taskListJson)
 })
 
 app.post('/api/deleteTask', (req, res) =>{
   var taskDel = req.body;
-  console.log(req.body, " <- Thats the printed result")
-  //taskDel = JSON.parse(taskDel);
   Task.deleteOne(taskDel, (err) => {
     if(err) return console.error(err);
+    console.log("deleted");
+    res.sendStatus(200);
   });
-  console.log("Deleted");
-  res.sendStatus(200);
 });
 
 app.post('/sendData', (req, res) => {
-  res.send('youre trying to send ddata!')
+  res.send('youre trying to send data!')
   console.log(req.body);
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
-// // db.once('open', () => {
-// //   // we're connected!
-// //   console.log('connected to db');
-
-// //   //access task from schemas.js
-// //   const Task = require('./schemas').task;
-
-// //   //create new task
-// //   //const TestTask = new Task({ title: 'testizzda', asdf: 1337});
-
-// //   //call methods of models like this
-// //   // TestTask.speak();
-
-// //   //save task to mongodb like this
-// //   // TestTask.save();
-
-// //   //a query would normally need to be executed after save has fully finished:
-// //   // TestTask.save().then(() => {//call find here})
-// //   // await could also work
-// //   Task.find((err, tasks) => {
-// //     if (err) return console.error(err);
-// //     console.log(tasks);
-// //   });
-
-// });
