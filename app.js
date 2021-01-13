8674c0954348bec8bf7a19a19f272df7b1738b23
Maximilian Saerov <47600203+mxsrv@users.noreply.github.com>
2021-01-13 14:00:51 +0100

    Feat/actions (#12)
    * Actions YAML
    
    * runs-on ubuntu
    
    * Folder renamed from worklflows to workflows
    
    * Adds some more specification
    
    * Work still in Progress
    
    * Work still in Progress
    
    * applied JavaScript Standard Style linter and formatter
    
    * .toArray() does not work
    
    * Fixed Linter issue
    
    Co-authored-by: remunds <raban.emunds@gmail.com>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-//general settings
-const boxName = 'pi1';
+// general settings
+const boxName = 'pi1'
 
-//express server
+// express server
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -12,16 +12,16 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 const port = 8000
 
-const schemas = require('./schemas');
-const Task = schemas.task;
-const Image = schemas.image;
+const schemas = require('./schemas')
+const Task = schemas.task
+const Image = schemas.image
 
-//mongoose controls our mongodb
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb://192.168.0.64/${boxName}`, { useNewUrlParser: true });
+// mongoose controls our mongodb
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb://192.168.0.64/${boxName}`, { useNewUrlParser: true })
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -33,59 +33,59 @@ app.get('/getData', (req, res) => {
 })
 
 app.get('/api/getTasks', (req, res) => {
-  Task.find((err, tasks) => {
-    if (err) return console.error(err);
-    console.log(tasks);
+  Task.find({}, (err, tasks) => {
+    if (err) return console.error(err)
+    console.log(tasks)
     res.send(tasks)
-  });
+  })
 })
 
 app.post('/api/deleteTask', (req, res) => {
-  var taskDel = req.body;
+  const taskDel = req.body
   Task.deleteOne(taskDel, (err) => {
-    if (err) return console.error(err);
-    console.log("deleted");
-    res.sendStatus(200);
-  });
-});
+    if (err) return console.error(err)
+    console.log('deleted')
+    res.sendStatus(200)
+  })
+})
 
 app.get('/api/getImage', (req, res) => {
   // get query, q.id = x if url ends with ?id=x
   //  var q = url.parse(req.url, true).query;
-  var id = req.query.id;
+  const id = req.query.id
   Image.findById(id, (err, image) => {
     if (err) return console.error(err)
-    res.send(image);
-  });
+    res.send(image)
+  })
 })
 
 app.post('/api/putLabel', (req, res) => {
-  var id = req.body.id;
-  var lab = req.body.label;
+  const id = req.body.id
+  const lab = req.body.label
 
-  Image.findByIdAndUpdate(id, { $push: { "label" : lab }}, {new : true, useFindAndModify : false}, function(err, result) {
-    if(err) console.log(err);
-    else{
-      res.send(result.label);
+  Image.findByIdAndUpdate(id, { $push: { label: lab } }, { new: true, useFindAndModify: false }, function (err, result) {
+    if (err) console.log(err)
+    else {
+      res.send(result.label)
     }
-  });
+  })
 })
 
 app.post('/api/putLabel', (req, res) => {
-  var id = req.body.id;
-  var lab = req.body.label;
- 
-  Image.findByIdAndUpdate(id, { $push: { "label" : lab }}, {new : true, useFindAndModify : false}, function(err, result){
-    if(err) console.log(err);
-    else{
-      res.send(result.label);
+  const id = req.body.id
+  const lab = req.body.label
+
+  Image.findByIdAndUpdate(id, { $push: { label: lab } }, { new: true, useFindAndModify: false }, function (err, result) {
+    if (err) console.log(err)
+    else {
+      res.send(result.label)
     }
-  });
+  })
 })
 
 app.post('/sendData', (req, res) => {
   res.send('youre trying to send data!')
-  console.log(req.body);
+  console.log(req.body)
 })
 
 app.listen(port, () => {
