d0e9fac4e8485d3f252f1252dc7f74d7d394cdb0
remunds <61607050+remunds@users.noreply.github.com>
2021-01-13 15:46:56 +0100

    Feat/generate tasks node + feat/fastUpload (#7)
    * wip taskgenerator
    
    wip generating tasks
    
    added imageSchema and /getImage route which sends an image as http response
    
    added /putLabel route and tests
    
    done
    
    wip
    
    wip
    
    wip
    
    wip
    
    donw
    
    review changes
    
    * adds standard to check for js standards to action
    
    * action: standard
    
    * adds mongodb to action

--- app.js
+++ app.js
@@ -3,91 +3,132 @@ const boxName = 'pi1'
 
 // express server
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 app.use(express.json())
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-app.use(bodyParser.json())
 const port = 8000
 
 const schemas = require('./schemas')
 const Task = schemas.task
 const Image = schemas.image
+const Chunk = schemas.chunk
+const File = schemas.file
 
-// mongoose controls our mongodb
-const mongoose = require('mongoose')
-mongoose.connect(`mongodb://192.168.0.64/${boxName}`, { useNewUrlParser: true })
+async function getHighestPriorityFile () {
+  let highestPriority
+  // find oldest and least downloaded chunk + file
+  const priorityChunk = await Chunk.findOne().sort({ downloads: 1, timestamp: 1 }).exec()
+  const priorityFile = await File.findOne().sort({ downloads: 1, uploadDate: 1 }).exec()
+  if (priorityChunk.downloads == null || priorityFile.downloads == null ||
+    priorityChunk.timestamp == null || priorityFile.uploadDate == null) {
+    return -1
+  }
+  if (priorityChunk.downloads > priorityFile.downloads) {
+    highestPriority = priorityFile
+    // count downloads up
+    File.updateOne({ _id: priorityFile.id }, { downloads: priorityFile.downloads + 1 }).exec()
+  } else if (priorityChunk.downloads < priorityFile.downloads) {
+    highestPriority = priorityChunk
+    Chunk.updateOne({ _id: priorityChunk.id }, { downloads: priorityChunk.downloads + 1 }).exec()
+  } else {
+    if (priorityChunk.timestamp >= priorityFile.uploadDate) {
+      highestPriority = priorityFile
+      File.updateOne({ _id: priorityFile.id }, { downloads: priorityFile.downloads + 1 }).exec()
+    } else {
+      highestPriority = priorityChunk
+      Chunk.updateOne({ _id: priorityChunk.id }, { downloads: priorityChunk.downloads + 1 }).exec()
+    }
+  }
+  // copy to delete the downloads property
+  const toReturn = ({ ...highestPriority }._doc)
+  delete toReturn.downloads
+  return toReturn
+}
 
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/register', (req, res) => {
+  res.send({ piID: boxName })
 })
 
-app.get('/getData', (req, res) => {
-  console.log('Getting Data!')
-  res.send('Getting Data!')
+app.get('/api/getData', async (req, res) => {
+  const fileToSend = await getHighestPriorityFile()
+  if (fileToSend === -1) {
+    res.status(400)
+    res.send({ error: 'could not find highest priority file' })
+  } else {
+    res.send(fileToSend)
+  }
 })
 
 app.get('/api/getTasks', (req, res) => {
   Task.find({}, (err, tasks) => {
-    if (err) return console.error(err)
-    console.log(tasks)
-    res.send(tasks)
+    if (err) {
+      res.status(400)
+      res.send({ error: 'database not available' })
+    }
+    if (tasks == null) {
+      res.status(400)
+      res.send({ error: 'no tasks defined' })
+    } else {
+      res.send(tasks)
+    }
   })
 })
 
 app.post('/api/deleteTask', (req, res) => {
-  const taskDel = req.body
-  Task.deleteOne(taskDel, (err) => {
-    if (err) return console.error(err)
-    console.log('deleted')
-    res.sendStatus(200)
+  Task.deleteOne(req.body, (err) => {
+    if (err) {
+      res.status(400)
+      res.send({ error: 'could not delete task' })
+    } else {
+      res.sendStatus(200)
+    }
   })
 })
 
 app.get('/api/getImage', (req, res) => {
   // get query, q.id = x if url ends with ?id=x
-  //  var q = url.parse(req.url, true).query;
-  const id = req.query.id
-  Image.findById(id, (err, image) => {
-    if (err) return console.error(err)
-    res.send(image)
-  })
-})
-
-app.post('/api/putLabel', (req, res) => {
-  const id = req.body.id
-  const lab = req.body.label
-
-  Image.findByIdAndUpdate(id, { $push: { label: lab } }, { new: true, useFindAndModify: false }, function (err, result) {
-    if (err) console.log(err)
-    else {
-      res.send(result.label)
+  Image.findById(req.query.id, (err, image) => {
+    if (err) {
+      res.status(400)
+      res.send({ error: 'database error' })
+    } else if (image == null) {
+      res.status(400)
+      res.send({ error: 'could not find image in database' })
+    } else {
+      res.send(image)
     }
   })
 })
 
 app.post('/api/putLabel', (req, res) => {
-  const id = req.body.id
-  const lab = req.body.label
-
-  Image.findByIdAndUpdate(id, { $push: { label: lab } }, { new: true, useFindAndModify: false }, function (err, result) {
-    if (err) console.log(err)
-    else {
+  // post query, q.id = x; q.label = y if url ends with ?id=x&?label=y
+  Image.findByIdAndUpdate(req.body.id, { $push: { label: req.body.label } }, { new: true, useFindAndModify: false }, (err, result) => {
+    if (err) {
+      res.status(400)
+      res.send({ error: 'database error' })
+    }
+    if (result == null) {
+      res.status(400)
+      res.send({ error: 'could not find image in database' })
+    } else {
       res.send(result.label)
     }
   })
-})
-
-app.post('/sendData', (req, res) => {
-  res.send('youre trying to send data!')
-  console.log(req.body)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`listening at http://localhost:${port}`)
+})
+
+// mongoose controls our mongodb
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb://localhost/${boxName}`, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  // we're connected!
+  console.log('connected to db')
+  // generate tasks
+  const generator = require('./taskgenerator')
+  generator.apply()
 })
--- schemas.js
+++ schemas.js
@@ -2,14 +2,36 @@ const mongoose = require('mongoose')
 
 // create schema with all needed fields
 const taskSchema = new mongoose.Schema({
+  // careful: title and imageId(if != null) need to be unique
   title: String,
-  description: String
+  description: String,
+  imageId: String
 })
 
 const imageSchema = new mongoose.Schema({
   type: String,
   data: Buffer,
   label: []
+})
+
+const chunkSchema = new mongoose.Schema({
+  timestamp: Number,
+  downloads: Number,
+  files_id: String,
+  n: Number,
+  data: [Buffer]
+})
+
+const fileSchema = new mongoose.Schema({
+  downloads: Number,
+  length: Number,
+  chunkSize: Number,
+  uploadDate: Number,
+  md5: String,
+  filename: String,
+  contentType: String,
+  aliases: [String],
+  metadate: Buffer // any
 })
 
 /* add methods like this:
@@ -25,3 +47,5 @@ const imageSchema = new mongoose.Schema({
 // export the mongoose model with the name Task (creates collection tasks)
 module.exports.task = mongoose.model('Task', taskSchema)
 module.exports.image = mongoose.model('Image', imageSchema)
+module.exports.chunk = mongoose.model('Chunk', chunkSchema)
+module.exports.file = mongoose.model('File', fileSchema)
